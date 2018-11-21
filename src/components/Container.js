2d246affe6e1b082b59466bc1f09541f03e5b4b6
tharnadek <tharnadek@gmail.com>
2018-11-21 15:17:22 -0800

    add some structure

---
+++ src/components/Container.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Container extends Component {
+    render () {
+        return (
+            <p>Container!</p>
+        )
+    }
+}
+
+export default Container
